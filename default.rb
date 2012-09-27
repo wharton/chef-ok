65d9ee34659ec94573dddd98e207b67b236283f7
Courtney Wilburn <wilburnc@wharton.upenn.edu>
2012-09-27 15:24:55 -0400

    corrected path in download location

---
+++ default.rb
@@ -0,0 +1,65 @@
+#
+# Cookbook Name:: ok
+# Recipe:: default
+#
+# Copyright 2012, Courtney Wilburn
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+#install unzip if needed
+
+package "unzip" do
+  action :install
+end
+
+# download ok
+
+remote_file "#{Chef::Config['file_cache_path']}/ok-0.1.zip" do
+  source "#{node['ok']['download']['url']}"
+  action :create_if_missing
+  mode "0744"
+  owner "root"
+  group "root"
+  not_if { File.directory?("#{node['ok']['install_path']}/ok") }
+end
+
+
+# Extract archive
+
+script "install_ok" do
+  interpreter "bash"
+  user "root"
+  cwd "#{Chef::Config['file_cache_path']}"
+  code <<-EOH
+unzip ok-0.1.zip 
+mv ok #{node['ok']['install_path']}
+chown -R nobody:bin #{node['ok']['install_path']}/ok
+EOH
+  not_if { File.directory?("#{node['ok']['install_path']}/ok") }
+end
+
+# Set up ColdFusion mapping
+
+execute "start_cf_for_ok_default_cf_config" do
+  command "/bin/true"
+  notifies :start, "service[coldfusion]", :immediately
+end
+
+coldfusion902_config "extensions" do
+  action :set
+  property "mapping"
+  args ({ "mapName" => "/ok",
+          "mapPath" => "#{node['ok']['install_path']}/ok"})
+end
