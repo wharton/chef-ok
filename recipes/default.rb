31e9c7301363c1221e622f3db95667da4b5c7274
Courtney Wilburn <wilburnc@wharton.upenn.edu>
2012-10-15 16:31:43 -0400

    changing to version 10 of coldfusion config

--- recipes/default.rb
+++ recipes/default.rb
@@ -57,7 +57,7 @@ execute "start_cf_for_ok_default_cf_config" do
   notifies :start, "service[coldfusion]", :immediately
 end
 
-coldfusion902_config "extensions" do
+coldfusion10_config "extensions" do
   action :set
   property "mapping"
   args ({ "mapName" => "/ok",
