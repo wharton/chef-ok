267f3ff85f0a416f3a69dbc2fd9777e41ab477cb
Nathan Mische <nmische@gmail.com>
2013-02-14 09:48:56 -0500

    Changing default installation path back to /vagrant/frameworks.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "wilburnc@wharton.upenn.edu"
 license          "Apache 2.0"
 description      "Installs/Configures OK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 %w{ centos redhat ubuntu }.each do |os|
   supports os
