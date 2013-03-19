c9b0fabd9078e242af24cd47674c4cf065980e5c
Brian Flad <bflad417@gmail.com>
2013-03-19 17:37:32 -0400

    Added version attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@
 default['ok']['install_path'] = "/vagrant/frameworks"
 default['ok']['owner'] = nil
 default['ok']['group'] = "bin"
-default['ok']['download']['url'] = "https://github.com/nmische/cf-ok/archive/0.2.0.zip"
+default['ok']['version'] = "0.2.0"
+default['ok']['download']['url'] = "https://github.com/nmische/cf-ok/archive/#{node['ok']['version']}.zip"
