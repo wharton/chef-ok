b72e6181d2d494962c6b423ed6ab08bdebbc6078
Nathan Mische <nmische@gmail.com>
2013-03-19 15:17:23 -0400

    Updating to download latest version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@
 default['ok']['install_path'] = "/vagrant/frameworks"
 default['ok']['owner'] = nil
 default['ok']['group'] = "bin"
-default['ok']['download']['url'] = "https://github.com/nmische/cf-ok/archive/0.1.0.zip"
+default['ok']['download']['url'] = "https://github.com/nmische/cf-ok/archive/0.2.0.zip"
